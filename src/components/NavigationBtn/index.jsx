421afea7ec926ba6bcef22164794089ab3ac33c0
Nitin Chaudhary <76252414+nitin-io@users.noreply.github.com>
2024-02-11 14:49:43 +0530

    Made navigation button to add  class dynamically

--- src/components/NavigationBtn/index.jsx
+++ src/components/NavigationBtn/index.jsx
@@ -1,9 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./navigationBtn.css";
 
-const NavigationBtn = ({ title, path, active }) => {
+const NavigationBtn = ({ title, path }) => {
+  const { pathname } = useLocation();
+
   return (
-    <div className={`navigation-btn ${active && "active"}`}>
+    <div
+      className={pathname === path ? "navigation-btn active" : "navigation-btn"}
+    >
       <Link to={path}>{title}</Link>
     </div>
   );
